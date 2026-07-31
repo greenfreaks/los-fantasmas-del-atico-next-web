33c02e60511b95a65dd7a7ad1fa02aeaa10b9cd4
Mario Sandoval Velázquez <mario.sandoval@fonyou.com>
2026-07-31 17:41:46 -0600

    ADD. Se agrega lógica y textos
    - Se agrega lógica para definir si se muestra una sección o no.
    
    - Se añaden links de las redes sociales
    
    - Se Establecen los textos reales de los miembros
    
    - Se añade botón de unicación y costo del evento

--- src/app/(main)/home/components/sideSocial.tsx
+++ src/app/(main)/home/components/sideSocial.tsx
@@ -1,14 +1,41 @@
 import Image from "next/image"
+import { whatsappMessage } from "../utils/whatsapp-message"
 import facebook from "@/src/assets/icons/facebook.webp"
 import instagram from "@/src/assets/icons/instagram.webp"
 import whatsapp from "@/src/assets/icons/whatsapp.webp"
+import youtube from "@/src/assets/icons/youtube.webp"
+
+interface SideSocialProps{
+  icon: string
+  url: string
+}
+const SOCIAL_LIST: SideSocialProps[] = [
+  {
+    icon: facebook.src,
+    url: "https://www.facebook.com/losfantasmasdelatico",
+  },
+  {
+    icon: instagram.src,
+    url: "https://www.instagram.com/los_fantasmas_del_atico?igsh=MXRzdTU4MHhybmhhcQ==",
+  },
+  {
+    icon: whatsapp.src,
+    url: whatsappMessage(),
+  },
+  {
+    icon: youtube.src,
+    url: "https://www.youtube.com/@LosFantasmasdel%C3%81tico",
+  }
+];
 
 export const SideSocial = () => {
   return (
     <div className="flex gap-x-4 sm:flex-col sm:gap-y-4">
-      <Image src={facebook} alt="Facebook" width={30} height={30} />
-      <Image src={instagram} alt="Instagram" width={30} height={30} />
-      <Image src={whatsapp} alt="Instagram" width={30} height={30} />
+      {SOCIAL_LIST.map((social, index) => (
+        <a href={social.url} key={index} target="_blank" rel="noopener noreferrer">
+          <Image src={social.icon} alt="Social Icon" width={30} height={30} />
+        </a>
+      ))}
     </div>
   )
 }
